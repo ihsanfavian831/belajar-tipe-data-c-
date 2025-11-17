bc235c7a5b448d83158e1bfe6a23439c7589bbd7
ihsan favian <ihsanfavian831@gmail.com>
2025-11-17 13:37:51 +0700

    Add files via upload

---
+++ tipedatachar.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char buah;
+    buah = 'd';
+    cout <<"ini adalah buah:" << buah << endl;
+
+    return 0;
+}
---
+++ tipedatafloat.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float angka1;
+    double angka2;
+
+    angka1 = 5,55;
+    angka2 = 89;
+
+    cout << "angka (float) pendek:" << angka1 << endl;
+    cout << "angka (double) nilai:" << angka2 << endl;
+
+    return 0;
+}
