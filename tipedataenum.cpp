465043f8cc6fd9c1cac09ef54392479602323c3e
ihsan favian <ihsanfavian831@gmail.com>
2025-11-21 12:59:45 +0700

    Add files via upload

---
+++ tipedataenum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+enum nama_zodiak {capricorn, aquarius, taurus, pisces, aries, gemini, leo, virgo, libra, scorpio, sagitarius};
+
+int main ()
+{
+	nama_zodiak var1;
+	
+	var1 = capricorn;
+	cout << var1 << endl;
+	
+	var1 = aquarius;
+	cout << var1 << endl;
+	
+	var1 = virgo;
+	cout << var1 << endl;
+	
+	var1 = taurus;
+	cout << var1 << endl;
+	
+	return 0;
+}
